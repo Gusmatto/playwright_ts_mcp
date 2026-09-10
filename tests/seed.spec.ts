49691fabf618c4a865b190bd439e8c4df5835064
Gusmatto <gustavo.matto46@gmail.com>
2026-09-09 22:00:19 -0300

    test: add seed for automationexercise.com

--- tests/seed.spec.ts
+++ tests/seed.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Test group', () => {
   test('seed', async ({ page }) => {
-    // generate code here.
+    await page.goto('https://www.automationexercise.com/');
+    await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
   });
 });
